815f2fcd8dd8da55eff342fa5068cbb0c798e286
Kalakrishnankr <kalakrishnankr@gmail.com>
2017-06-07 12:24:17 +0530

    Add files via upload

---
+++ Memberdetails.java
@@ -0,0 +1,33 @@
+package com.yalantis.euclid.library;
+
+/**
+ * Created by sabodhbs on 12/10/15.
+ */
+public class Memberdetails {
+    public String Sr_No;
+    public String Roll_No;
+    public String Name;
+    public String Address;
+    public String Mob_Num;
+    public String Present_Location;
+    public String Occupation;
+    public String Company_Designation;
+    public String E_Mail_ID;
+    public String Image_path;
+    public String Status_msg;
+
+    public Memberdetails(){
+        Sr_No  = "";
+        Roll_No = "";
+        Name = "";
+        Address = "";
+        Mob_Num = "";
+        Present_Location = "";
+        Occupation = "";
+        Company_Designation = "";
+        E_Mail_ID = "";
+        Image_path = null;
+        Status_msg = "";
+    }
+
+}
